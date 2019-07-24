0c02cd0b151fc387c81717229a07bd3fe3bde965
dj0nny <frank96990@gmail.com>
2019-07-24 14:25:07 +0200

    :sparkles: app models.d.ts

---
+++ client/src/store/models.d.ts
@@ -0,0 +1,12 @@
+export interface articlesResponse {
+  id: number;
+  title: string;
+  summary: string;
+  body: string;
+}
+
+export interface writersResponse {
+  id: number;
+  firstName: string;
+  secondName: string;
+}
